b210dee9e1aa64a1754702093dab90491044ce9a
alieveldar <illuminatiday@gmail.com>
2019-02-13 17:08:29 +0300

    #002 Fix errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-    $(".NewsSend").on("click", function(event) {
+    $(".NewsSend").on("click", function(event) {        
         var name = $(".JournalistName").val();
         var news = $(".NewsText").val();
         var picture = $("#sortpicture").prop('files')[0];
         var form_data = new FormData();
         form_data.append('file', picture);
         form_data.append('name', name);
-        form_data.append('text', news);
+        form_data.append('text', news);                       
         $.ajax({
             url: 'uploadnews.php',
             dataType: 'text',
@@ -16,7 +16,7 @@ $(document).ready(function() {
             data: form_data,
             type: 'post',
             success: function(data) {
-                toastr.info(data);
+                toastr.error(data);
                 $(".JournalistName").val("");
                 $(".NewsText").val("");
                 $("#sortpicture").val("");
@@ -29,7 +29,7 @@ $(document).ready(function() {
         var NewsId = $(event.target).data("id");
         $.post("deletenews.php", { id: NewsId})
             .done(function(data) {
-                toastr.info(data);
+                toastr.error(data);
                 $(".article" + NewsId).hide();
             });
     });
--- uploadnews.php
+++ uploadnews.php
@@ -2,10 +2,10 @@
 require_once 'conf/config.php';
 $name = $_POST['name'];
 $news = $_POST['text'];
-if (0 < $_FILES['file']['error']) {
-	exit;
+if (is_null($_FILES['file'])) {
+	echo "Необходимо выбрать фотографию";
+	die();
 }
-//if (is_uploaded_file($_FILES["filename"]["tmp_name"])) {
 if (file_get_contents($_FILES['file']['tmp_name'])) {
 	move_uploaded_file($_FILES['file']['tmp_name'], "assets/images/" . $_FILES['file']['name']);
 	$filename = $_FILES['file']['name'];
@@ -14,13 +14,21 @@ if (file_get_contents($_FILES['file']['tmp_name'])) {
 
 function add_news($name, $text, $filename, $connectEDB) {
 	$rez = '';
+	$name = mysqli_real_escape_string($connectEDB, sanitize_xss($name));
+	$text = mysqli_real_escape_string($connectEDB, sanitize_xss($text));
+	$filename = sanitize_xss($filename);
 	$filename = "assets/images/" . $filename;
 	$sql = "INSERT INTO articles (journalist, news_text, image) VALUES ('$name', '$text', '$filename')";
+
 	if (mysqli_query($connectEDB, $sql)) {
 		$rez = 'Новость добавлена';
 	} else {
-		$rez = 'Новость не добавлена, ошибка базы данных!';
+		$rez = 'Новость не добавлена, ошибка базы данных!' . mysqli_error($connectEDB);
 	}
 	mysqli_close($connectEDB);
 	return $rez;
 }
+function sanitize_xss($value) {
+	return htmlspecialchars(strip_tags($value));
+}
+die();
